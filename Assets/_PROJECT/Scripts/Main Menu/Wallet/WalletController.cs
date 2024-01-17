6ec1ffe3351c134d6866d85738ded669439c5a1a
Ivaylo Palchev <ivopalchev@gmail.com>
2024-01-17 19:13:23 +0200

    new implementation of wallet animations

--- Assets/_PROJECT/Scripts/Main Menu/Wallet/WalletController.cs
+++ Assets/_PROJECT/Scripts/Main Menu/Wallet/WalletController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
@@ -10,37 +11,37 @@ public class WalletController : MonoBehaviour
     private GameObject cardPrefab;
     
     [SerializeField] 
-    private Transform cardsTransformViewCollapsed;
+    private Transform cardsHolderTransform;
     
-    [SerializeField] 
-    private Transform cardsTransformViewExpanded;
-    
-    [SerializeField]
-    private CardComponent currentCardComponent;
-
     [SerializeField]
     private ScrollRect expandedScrollRect;
     
     [SerializeField]
-    private VerticalLayoutGroup layoutGroup;
+    private VerticalLayoutGroup verticalLayoutGroup;
     
     [SerializeField]
-    private RectTransform expandedViewScrollRectTransform;
+    private RectTransform ScrollRectTransform;
+    
+    [SerializeField]
+    private ContentSizeFitter contentSizeFitterComponent;
+
+    [SerializeField]
+    private GameObject DeselectActiveCardButton;
     
     public void Start()
     {
+        DeselectActiveCardButton.SetActive(false);
+
         //Populate cards
         for (int i = 0; i < 10; i++)
         {
-            InstantiateCard(i, cardsTransformViewCollapsed);
-            InstantiateCard(i, cardsTransformViewExpanded);
+            InstantiateCard(i, cardsHolderTransform);
         }
 
         if (expandedScrollRect != null)
-            expandedScrollRect.verticalScrollbar.value = 1;
-        
-        cardsTransformViewCollapsed.gameObject.SetActive(false);
-        cardsTransformViewExpanded.gameObject.SetActive(true);
+        {
+            
+        }
     }
 
     void InstantiateCard(int id, Transform transform)
@@ -57,66 +58,130 @@ public class WalletController : MonoBehaviour
         }
     }
 
+    private Transform currentSelectedCardTransform;
+    float targetValue = 1f;
+    float duration = 0.1f;
+    private int childOriginalIndex = 0;
+    private float originalSpacing;
+    private int originalBottomPadding;
     
     public void SelectCurrentActiveCard(CardComponent cardComponent)
     {
-        expandedScrollRect.enabled = true;
-        //cardsTransformViewCollapsed.gameObject.SetActive(true);
+        currentSelectedCardTransform = cardComponent.transform;
+        childOriginalIndex = currentSelectedCardTransform.GetSiblingIndex();
 
-        float currentSpacing = layoutGroup.spacing;
+        originalSpacing = verticalLayoutGroup.spacing;
+        originalBottomPadding = verticalLayoutGroup.padding.bottom;
+        
+        //Set children as first
+        currentSelectedCardTransform.SetAsFirstSibling();
+        
+        DOTween.To(() => expandedScrollRect.verticalScrollbar.value, 
+                x => expandedScrollRect.verticalScrollbar.value = x, 
+                targetValue, 
+                duration)
+            .OnComplete(() =>
+            {
+                // This lambda function is called when the animation is complete
+                // You can perform additional actions here if needed
+                expandedScrollRect.verticalScrollbar.value = 1;
 
-// Use DoTween to animate the spacing attribute
-        DOTween.To(() => currentSpacing, x => layoutGroup.spacing = x, -832, 0.3f)
-            .SetEase(Ease.OutQuad)
-            .OnComplete(() => {
-                // This will be called when the spacing animation is complete
-                expandedScrollRect.enabled = false;
-                
-                Vector2 currentRectTransformExpandedView = expandedViewScrollRectTransform.localPosition;
-                float destinationRectTransformExpandedViewY = 500; // Replace this with your desired value
+                currentSelectedCardTransform.SetParent(expandedScrollRect.transform);
+                currentSelectedCardTransform.SetAsFirstSibling();
 
-// Use DoTween to animate the localPosition.y attribute
-                DOTween.To(() => currentRectTransformExpandedView.y, x => currentRectTransformExpandedView.y = x, destinationRectTransformExpandedViewY, 0.3f)
-                    .OnUpdate(() => {
-                        // Apply the updated localPosition during each update
-                        expandedViewScrollRectTransform.localPosition = currentRectTransformExpandedView;
-                    })
-                    .SetEase(Ease.OutQuad);
-                
+                //Content Size fitter
+                contentSizeFitterComponent.enabled = false;
+        
+                //Vertical layout group
+                verticalLayoutGroup.enabled = true;
+
+                AnimateLayoutSpacing(-717, true);
+
+                ScaleDownChildren(cardsHolderTransform);
             });
     }
 
+    float initialScalePercentage = 1.0f; // Initial scale percentage
+    private float scaleReduction = 0.02f;
+    
+    void ScaleDownChildren(Transform parent)
+    {
+        int childCount = parent.childCount;
+
+        for (int i = childCount - 1; i >= 0; i--)
+        {
+            Transform child = parent.GetChild(i);
+            float scalePercentage = initialScalePercentage - (childCount - 1 - i) * scaleReduction;
+
+            // Use DoTween to scale the child down along the X-axis only
+            child.DOScaleX(child.localScale.x * scalePercentage, 0.1f)
+                .SetEase(Ease.OutQuad);
+        }
+    }
+    
+    void RestoreAllScales(Transform parent)
+    {
+        int childCount = parent.childCount;
+        for (int i = 0; i < childCount; i++)
+        {
+            Transform child = parent.GetChild(i);
+
+            // Reset the scale to (1, 1, 1)
+            child.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+
+    void AnimateLayoutSpacing(float endValue, bool activateDeselectButton, Action onCompleteCallback = null)
+    {
+        float currentSpacing = verticalLayoutGroup.spacing;
+        
+        DOTween.To(() => currentSpacing, x => verticalLayoutGroup.spacing = x, endValue, 0.2f)
+            .SetEase(Ease.OutQuad)
+            .OnComplete(() =>
+            {
+                DeselectActiveCardButton.SetActive(activateDeselectButton);
+                
+                onCompleteCallback?.Invoke();
+            });
+    }
+    
+    // void AnimateLayoutBottomPadding(float endValue)
+    // {
+    //     RectOffset currentPadding = verticalLayoutGroup.padding;
+    //     
+    //     DOTween.To(() => currentPadding.bottom, x => currentPadding.bottom = x, endValue, 0.2f)
+    //         .SetEase(Ease.OutQuad)
+    //         .OnUpdate(() =>
+    //         {
+    //             // Apply the updated padding during the animation
+    //             verticalLayoutGroup.padding = currentPadding;
+    //         })
+    //         .OnComplete(() =>
+    //         {
+    //             // Animation complete logic, if needed
+    //         });
+    // }
+
     public void DeselectCurrentActiveCard()
     {
-        expandedScrollRect.enabled = true;
-        //cardsTransformViewCollapsed.gameObject.SetActive(false);
+        if (currentSelectedCardTransform == null)
+            return;
         
-        float currentSpacing = layoutGroup.spacing;
+        RestoreAllScales(cardsHolderTransform);
+            
         
-        // Use DoTween to animate the spacing attribute
-        DOTween.To(() => currentSpacing, x => layoutGroup.spacing = x, -565, 0.3f)
-            .SetEase(Ease.OutQuad); // You can change the ease type as per your preference
-        //expandedViewAnimator.SetBool("Collapse", false);
+        AnimateLayoutSpacing(originalSpacing, false, () =>
+        {
+            currentSelectedCardTransform.SetParent(cardsHolderTransform);
+            Debug.LogError(currentSelectedCardTransform.parent, currentSelectedCardTransform.parent.gameObject);
+            currentSelectedCardTransform.SetSiblingIndex(childOriginalIndex);
+            
+            contentSizeFitterComponent.enabled = true;
 
+            verticalLayoutGroup.enabled = true;
+        });
         
-        Vector2 currentRectTransformExpandedView = expandedViewScrollRectTransform.localPosition;
-        float destinationRectTransformExpandedViewY = -50; // Replace this with your desired value
-
-// Use DoTween to animate the localPosition.y attribute
-        DOTween.To(() => currentRectTransformExpandedView.y, x => currentRectTransformExpandedView.y = x, destinationRectTransformExpandedViewY, 0.3f)
-            .OnUpdate(() => {
-                // Apply the updated localPosition during each update
-                expandedViewScrollRectTransform.localPosition = currentRectTransformExpandedView;
-            })
-            .SetEase(Ease.OutQuad);
         
-        //collapsedViewAnimator.SetBool("Expand", true);
-
-        //if (expandedScrollRect != null)
-            //expandedScrollRect.verticalScrollbar.value = 1;
-        
-        //cardsTransformViewCollapsed.gameObject.SetActive(false);
-        //cardsTransformViewExpanded.gameObject.SetActive(true  );
     }
     
 }
